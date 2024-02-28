75e420dd9d588c1d4d9a9eb4188a5c65a03a88ba
Zak <netuser.ucsm.zmt@gmail.com>
2024-02-28 19:17:44 +0700

    Models & Migrations

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Role Permission extends Model
+class RolePermission extends Model
 {
     use HasFactory;
 }
