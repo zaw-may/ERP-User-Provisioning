8b0166a1c7bd0da93e4504234f5e366857de98bf
Zak <netuser.ucsm.zmt@gmail.com>
2024-03-11 19:23:54 +0700

    Edited View

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class = "container">
+        <h1>Welcome to the Dashboard</h1>
+        <div class = "row">
+            <div class = "col-md-6">
+                <div class = "card">
+                    <div class = "card-header">
+                        Dashboard Section 1
+                    </div>
+                    <div class = "card-body">
+                        <p>Dashboard Content</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class = "col-md-6">
+                <div class = "card">
+                    <div class = "card-header">
+                        Dashboard Section 2    
+                    </div>
+                    <div class = "card-body">
+                        <p>Dashboard Content</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
